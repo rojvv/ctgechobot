1b0ac70b860357960557993b9da62bd38487fc31
Roj <ez@roj.im>
2023-03-24 19:58:43 +0300

    Add link to source

--- curl_utils.h
+++ curl_utils.h
@@ -6,6 +6,25 @@
 
 #define API_URL "https://api.telegram.org"
 
+void check_curl(CURLcode code) {
+  if (code != 0) {
+    fprintf(stderr, "[CURL] %s\n", curl_easy_strerror(code));
+    exit(1);
+  }
+}
+
+void check_curlu(CURLUcode code) {
+  if (code != 0) {
+    fprintf(stderr, "[CURLU] %s\n", curl_url_strerror(code));
+    exit(1);
+  }
+}
+
+char *get_url(char *token, char *method) {
+  return curl_maprintf("https://api.telegram.org/bot%s/%s", token, method);
+}
+
+// https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html#EXAMPLE
 typedef struct Response {
   char *content;
   size_t size;
@@ -25,22 +44,4 @@ static size_t write_function(void *data, size_t size, size_t nmemb,
   response->content[response->size] = 0;
 
   return realsize;
-}
-
-void check_curl(CURLcode code) {
-  if (code != 0) {
-    fprintf(stderr, "[CURL] %s\n", curl_easy_strerror(code));
-    exit(1);
-  }
-}
-
-void check_curlu(CURLUcode code) {
-  if (code != 0) {
-    fprintf(stderr, "[CURLU] %s\n", curl_url_strerror(code));
-    exit(1);
-  }
-}
-
-char *get_url(char *token, char *method) {
-  return curl_maprintf("https://api.telegram.org/bot%s/%s", token, method);
 }
